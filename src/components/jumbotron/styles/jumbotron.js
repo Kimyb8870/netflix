908f2c126ee779c7bf0b216ff6ad21e33e584c12
kimyb8870 <qhd0084@gmail.com>
2021-02-15 15:31:22 +0900

    Fix: jumbotron padding unit error and typo error

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -3,7 +3,7 @@ import styled from "styled-components/macro";
 export const Item = styled.div`
   display: flex;
   border-bottom: 8px solid #222;
-  padding: 50% 5%;
+  padding: 50px 5%;
   color: white;
   overflow: hidden;
 `;
