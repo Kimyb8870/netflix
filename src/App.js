5a58b5d1347cc73ee064aa932d9ed7997a64d75a
kimyb8870 <qhd0084@gmail.com>
2021-02-18 05:50:24 +0900

    Set: accordion modulization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { JumbotronContainer } from "../src/containers/jumbotron";
 import { FooterContainer } from "../src/containers/footer";
+import { FaqsContainer } from "../src/containers/faqs";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
