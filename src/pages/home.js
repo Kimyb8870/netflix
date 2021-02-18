7e22b74a654b21d2558ad9c3f35fc4d610453180
kimyb8870 <qhd0084@gmail.com>
2021-02-19 05:51:54 +0900

    Set: header modulization

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,29 @@ import React from "react";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
+import { HeaderContainer } from "../containers/header";
+import { Feature, OptForm } from "../components";
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programms and more</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your eamil to create ro restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
