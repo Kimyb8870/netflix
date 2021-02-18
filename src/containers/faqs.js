027abfd8f987574643b405679a76cb420227f1cf
kimyb8870 <qhd0084@gmail.com>
2021-02-18 16:33:49 +0900

    Set: opt-form modulization

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,6 @@
 import faqsData from "../fixtures/faqs.json";
 import React from "react";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 
 export function FaqsContainer() {
   return (
@@ -12,7 +12,14 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
-      <Accordion.Frame></Accordion.Frame>
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your eamil to create ro restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
