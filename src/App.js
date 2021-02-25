57302589f00ddaeb1b3977cbfc28ef7d4a2ce5df
kimyb8870 <qhd0084@gmail.com>
2021-02-26 03:39:48 +0900

    Set: protect routes depends on user state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  // const user = { name: "kimyb" };
+  const user = null;
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SING_IN}>
-        <Signin />
-      </Route>
-      <Route exact path={ROUTES.SING_UP}>
-        <Signup />
-      </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          path={ROUTES.SING_IN}
+          loggedInPath={ROUTES.BROWSE}
+          exact
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          path={ROUTES.SING_UP}
+          loggedInPath={ROUTES.BROWSE}
+          exact
+        >
+          <Signup />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          path={ROUTES.HOME}
+          loggedInPath={ROUTES.BROWSE}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+      </Switch>
     </Router>
   );
 }
