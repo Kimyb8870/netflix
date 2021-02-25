7da920901dd94361cf0c1d9aeb0182d73da97829
kimyb8870 <qhd0084@gmail.com>
2021-02-25 14:34:10 +0900

    Feature: sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
 
 export default function Signin() {
-  return <p>hello from the sign in</p>;
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //check form input elements are valid
+  //email, password
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    //firebase work here!
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix?{" "}
+            <Form.Link to={ROUTES.SING_UP}>Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
